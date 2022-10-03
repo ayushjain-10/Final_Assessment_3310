a8d4a8f4e8dfedbab7bc946de9d0a2d61e4467b3
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-10-03 11:21:22 -0700

    update

--- number.js
+++ number.js
@@ -1,7 +1,7 @@
 function formatPhoneNumber(str) {
     var Str = str.trim();
     if (Str.length == 10) {
-        return '(' + Str[0] + Str[1] + Str[2] + ') ' + Str[3] + Str[4] + Str[5] + '-' + Str[6] + Str[7] + Str[8] + Str[9];
+        return '(' + Str.slice(0,3) + ') ' + Str.slice(3,6) + '-' + Str.slice(6,10);
     }
     else {
         return "Invalid Phone Number";
@@ -10,5 +10,5 @@ function formatPhoneNumber(str) {
 ;
 formatPhoneNumber('9809142333');
 //To run this file, uncomment the following line
-// console.log(formatPhoneNumber('9809142333'));
+console.log(formatPhoneNumber('9809142333'));
 module.exports.formatPhoneNumber = formatPhoneNumber;
